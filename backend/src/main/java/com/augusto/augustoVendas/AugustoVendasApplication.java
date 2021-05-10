e316a8bbc6fe4324520a828bd879f5ed6866f10a
AugustoHide <augustohide@gmail.com>
2021-05-10 14:27:39 -0400

    Projeto criado

---
+++ backend/src/main/java/com/augusto/augustoVendas/AugustoVendasApplication.java
@@ -0,0 +1,13 @@
+package com.augusto.augustoVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugustoVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AugustoVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/augusto/augustoVendas/AugustoVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augusto.augustoVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AugustoVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
