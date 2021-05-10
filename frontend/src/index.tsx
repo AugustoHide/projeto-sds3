7ab45f3b2468d1c4a5ffa9f8b05131844900c9e8
AugustoHide <augustohide@gmail.com>
2021-05-10 18:43:30 -0400

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'C:/workspace/porjeto-sds3/frontend/src/assets/css/styles.css';
 import App from './App';
 
 ReactDOM.render(
