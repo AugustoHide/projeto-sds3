4738e7265ab90da141e607a3355c2feabbdc1ae6
AugustoHide <augustohide@gmail.com>
2021-05-14 16:43:45 -0400

    DonutChar Integration

--- frontend/src/components/DonutsChart/index.tsx
+++ frontend/src/components/DonutsChart/index.tsx
@@ -1,10 +1,33 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 const DonutsChart = () => {
+
+    let chartdata: ChartData = { labels: [], series: [] };
+
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then((response) => {
+            const data = response.data as SaleSum[]; //converte backend pra formato no frontemd
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum); // p/ cada x no saleSum eu vou pegar x.sum
+
+            chartdata = { labels: myLabels, series: mySeries };
+            console.log(chartdata);
+        });
+
+    /*
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
+    */
 
     const options = {
         legend: {
@@ -14,8 +37,8 @@ const DonutsChart = () => {
 
     return (
         <Chart
-            options={{ ...options, labels: mockData.labels }}
-            series={mockData.series}
+            options={{ ...options, labels: chartdata.labels }}
+            series={chartdata.series}
             type="donut"
             height="240"
         />
