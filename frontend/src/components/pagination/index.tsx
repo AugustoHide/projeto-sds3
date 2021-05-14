5f585b730029e32e0e07de6134d697c492123c39
AugustoHide <augustohide@gmail.com>
2021-05-14 18:34:42 -0400

    Pagination

---
+++ frontend/src/components/pagination/index.tsx
@@ -0,0 +1,28 @@
+import { SalePage } from "types/sale";
+
+type Props = {
+    page: SalePage;
+    onPageChage: Function;
+}
+// desestrututra entrada de função e monta como Props
+const Pagination = ({ page, onPageChage }: Props) => {
+    return (
+        <div className="row d-flex justify-content-center">
+            <nav>
+                <ul className="pagination">
+                    <li className={`page-item ${page.first ? 'disabled' : ''}`}>
+                        <button className="page-link" onClick={() => onPageChage(page.number - 1)}>Anterior</button>
+                    </li>
+                    <li className="page-item disabled">
+                        <span className="page-link">{page.number + 1}</span>
+                    </li>
+                    <li className={`page-item ${page.last ? 'disabled' : ''}`}>
+                        <button className="page-link" onClick={() => onPageChage(page.number + 1)}>Próxima</button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Pagination;
