a3d3d5375a0594a0d35efa99a779127d882832c9
AugustoHide <augustohide@gmail.com>
2021-05-14 16:05:16 -0400

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,9 @@
-import DonutsChart from "components/DonutsChart";
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import Footer from "components/Footer";
-import NavBar from "components/NavBar";
+import Routes from "Routes";
 
 ///**  A ideia de fazer NavBar separado é pra ajudar a reutilizar os componentes em outras páginas / lugares do projeto **/
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-
-        <div className="row px-13">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas as vendas</h5>
-            <DonutsChart />
-          </div>
-
-        </div>
-        <div className="py-3">
-          <h2 className="text-primary">Todas as vendas</h2>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+    <Routes />
   );
 }
 
